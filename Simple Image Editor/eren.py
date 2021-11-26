9e4445d0bcb9ea8eb8a5d4abab2a27306d839c3e
Eren Kumru <eren_kumru@hotmail.com>
2021-11-26 23:11:09 +0300

    Initial project files

---
+++ Simple Image Editor/eren.py
@@ -0,0 +1,2 @@
+# Eren's initial work file
+# can create new files when necessary
---
+++ Simple Image Editor/main.py
@@ -0,0 +1,5 @@
+# Main project file
+# File structure will be revised later to be convenient
+
+# Run first verify the project is working properly!
+print("\n Hello world!\n I am working properly, thanks for checking!")
