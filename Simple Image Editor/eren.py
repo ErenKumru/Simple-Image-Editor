40d57ada104be3a5bb5dc632c9e05c2fcc88f050
Eren Kumru <eren_kumru@hotmail.com>
2021-12-13 22:44:40 +0300

    Bugfix and feedback improvement
    ShowImage() prints better information and shows grayscale and colorful images distinctly
    Fixed a bug in IsGrayScale() that raises error caused of trying to read a grayscale image as RGB

--- Simple Image Editor/eren.py
+++ Simple Image Editor/eren.py
@@ -8,6 +8,9 @@ import cv2
 
 
 def IsGrayScale():
+    if(len(image.shape) < 3): return True
+    if(image.shape[2] == 1): return True
+
     sourceImage = Image.fromarray(image, "RGB")
     width, height = sourceImage.size
     for i in range(width):
@@ -20,16 +23,22 @@ def IsGrayScale():
 
 def ConvertToGrayScale():
     if (IsGrayScale()):
-        print("The source image is already grayscale! Please be sure to give a proper colorful image.")
+        print("ConvertToGrayScale: The source image is already grayscale! Please be sure to give a proper colorful image.")
+        return image
     else:
-        print("The source image is converted to grayscale successfully.")
-        return cv2.cvtColor(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), cv2.COLOR_GRAY2RGB)
+        print("ConvertToGrayScale: The source image is converted to grayscale successfully.")
+        return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 
 def ShowImage(sourceImage):
-    print("Image received. Showing image.")
-    plot.imshow(sourceImage)
-    plot.show()
+    if(IsGrayScale()):
+        print("ShowImage: (1 channel) Grayscale image received. Showing image.")
+        plot.imshow(cv2.cvtColor(sourceImage, cv2.COLOR_GRAY2RGB))
+        plot.show()
+    else:
+        print("ShowImage: (3 channel) RGB image received. Showing image.")
+        plot.imshow(sourceImage)
+        plot.show()
 
 
 image = img.imread("Sharbat Gula, the Afghan Girl.jpg")
